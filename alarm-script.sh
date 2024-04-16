fea30403aa95b188be9f98c1a8db037cf5c6978d
wesley mbate <wmbate@meliora.tech>
2024-04-16 16:21:36 +0300

    reverted value to percentage of 20

--- alarm-script.sh
+++ alarm-script.sh
@@ -4,10 +4,9 @@ export XDG_RUNTIME_DIR="/run/user/1000"
 currentBatteryPercentage=$(upower -i /org/freedesktop/UPower/devices/DisplayDevice | grep "percentage" | awk '{print $2}')
 currentBatteryPercentage=${currentBatteryPercentage/\%/}
 currentBatteryState=$(upower -i /org/freedesktop/UPower/devices/DisplayDevice | grep "state" | awk '{print $2}')
-if [ $currentBatteryPercentage -lt 62 ] && [ $currentBatteryState == "discharging" ]
+if [ $currentBatteryPercentage -lt 20 ] && [ $currentBatteryState == "discharging" ]
 then
 mpg123 /home/meliora/alarm-sound.mp3 -l 2
 fi
 #to view CRON logs
 #grep CRON /var/log/syslog
-
